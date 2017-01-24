f89683a9560dd0f7088693488672c27b8b2de716
claytonsilva <clayton@xdevel.com.br>
2017-01-24 20:55:14 -0200

    fix class

--- idistrict.ts
+++ idistrict.ts
@@ -1,6 +1,6 @@
-import {IBaseModel} from './ibase-model'
+import { Interfaces } from 'js-data-dao'
 import {ILocation} from './ilocation'
-export interface IDistrict extends IBaseModel {
+export interface IDistrict extends Interfaces.IBaseModel {
   name: string
   location: ILocation
 }
